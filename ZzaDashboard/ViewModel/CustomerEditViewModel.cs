2f1391b63c76d2d2d83404e40a3fbc4d1a2a8e70
Tanya <tanyaryzhik33@gmail.com>
2020-01-16 20:04:35 +0200

    Add project files.

---
+++ ZzaDashboard/ViewModel/CustomerEditViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Zza.Data;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard.ViewModel
+{
+    public class CustomerEditViewModel
+    {
+        public Customer Customer { get; set; }
+        public ICustomersRepository Repository { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public CustomerEditViewModel()
+        {
+            this.Repository = new CustomersRepository();
+            this.SaveCommand = new RelayCommand(SaveCommandExecute, SaveCommandCanExecute);
+            GetCustomer();
+        }
+
+        public void GetCustomer()
+        {
+            this.Customer = this.Repository.GetCustomerAsync(new Guid("11DA4696-CEA3-4A6D-8E83-013F1C479618")).Result;
+        }
+
+        private async void SaveCommandExecute()
+        {
+           await this.Repository.UpdateCustomerAsync(this.Customer);
+        }
+
+        public bool SaveCommandCanExecute()
+        {
+            // if (!String.IsNullOrWhiteSpace(this.Customer.FirstName) || !String.IsNullOrWhiteSpace(this.Customer.LastName) || !String.IsNullOrWhiteSpace(this.Customer.Phone))
+            //    return true;
+
+            //return false;
+
+            if (String.IsNullOrWhiteSpace(this.Customer.FirstName) || String.IsNullOrWhiteSpace(this.Customer.LastName) || String.IsNullOrWhiteSpace(this.Customer.Phone))
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ ZzaDashboard/ViewModelLocator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace ZzaDashboard
+{
+    public static class ViewModelLocator
+    {
+        private const string PropertyName = "AutoWireViewModel";
+
+        public static bool GetAutoWireViewModel(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoWireViewModelProperty, value);
+        }
+
+        public static readonly DependencyProperty AutoWireViewModelProperty =
+            DependencyProperty.RegisterAttached(ViewModelLocator.PropertyName, 
+                                                typeof(bool), 
+                                                typeof(ViewModelLocator), 
+                                                new PropertyMetadata(false, AutoWireViewModelChanged));
+
+        private static void AutoWireViewModelChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(d)) 
+                return;
+
+            var viewType = d.GetType();
+            //var viewTypeName = viewType.FullName;
+            var viewModelTypeName = viewType + "Model";
+            var viewModelType = Type.GetType(viewModelTypeName);
+            var viewModel = Activator.CreateInstance(viewModelType);
+
+            ((FrameworkElement)d).DataContext = viewModel;
+        }
+    }
+}
